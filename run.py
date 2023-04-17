096938cbb3749465ef8f044e7a09c4fad1639494
simjeh <simjeh@naver.com>
2023-04-17 15:48:19 +0900

    xyz argv

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import os
 import sys
 modelname=sys.argv[1]
-os.system("openrave.py --database kinematicreachability --numthreads=12 --robot=rsc/"+modelname+".xml --xyzdelta=0.01")
+if sys.argv[2] == None:
+    xyzres = "0.06"
+else: 
+    xyzres = sys.argv[2]
+
+os.system("openrave.py --database kinematicreachability --numthreads=12 --robot=rsc/"+modelname+".xml --xyzdelta="+xyzres)
+
