c806e818f5962defa5c0828e45a71b2ba432bbc8
simjeh <simjeh@naver.com>
2023-04-17 15:19:07 +0900

    updates

---
+++ clone.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+"""this code copy entire folder from ../.openrave/ to .openrave/"""
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+copytree('../.openrave/', '.openrave/')
+print("done")
