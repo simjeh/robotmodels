8e6f7ebacba4163632373e70284d5be8286cae04
simjeh <simjeh@naver.com>
2023-04-17 15:40:12 +0900

    er fix

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os
 import sys
 modelname=sys.argv[1]
-os.system("openrave.py --database kinematicreachability --numtheread=12 --robot=rsc/"+modelname+".xml --xyzdelta=0.06 --output="+modelname+".txt")
+os.system("openrave.py --database kinematicreachability --numthreads=12 --robot=rsc/"+modelname+".xml --xyzdelta=0.01 --output="+modelname+".txt")
